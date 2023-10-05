15500df200fa360397fef75e9670e895a0e7f8e8
minsusun <poodding397@gmail.com>
2023-10-06 02:56:09 +0900

    [problem2]Edit markdown document&Wrote black-box test cases

--- homework2/hw2-problem2/src/test/java/edu/postech/csed332/homework2/AbstractMutableGraphTest.java
+++ homework2/hw2-problem2/src/test/java/edu/postech/csed332/homework2/AbstractMutableGraphTest.java
@@ -55,4 +55,160 @@ public abstract class AbstractMutableGraphTest<V extends Comparable<V>, G extend
     // TODO: write black-box test cases for each method of MutableGraph with respect to
     //  the specification, including the methods of Graph that MutableGraph extends.
 
+    @Test
+    void testAddEdgeSourceInVTargetInV() {
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        assertTrue(graph.addEdge(v1, v2));
+
+        assertTrue(graph.containsEdge(v1, v2));
+        assertTrue(graph.containsEdge(v2, v1));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceInVTargetNotInV() {
+        graph.addVertex(v1);
+        assertTrue(graph.addEdge(v1, v2));
+
+        assertTrue(graph.containsEdge(v1, v2));
+        assertTrue(graph.containsEdge(v2, v1));
+        assertTrue(graph.containsVertex(v2));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceNotInVTargetInV() {
+        graph.addVertex(v2);
+        assertTrue(graph.addEdge(v1, v2));
+
+        assertTrue(graph.containsEdge(v1, v2));
+        assertTrue(graph.containsEdge(v2, v1));
+        assertTrue(graph.containsVertex(v1));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceNotInVTargetNotInV() {
+        assertTrue(graph.addEdge(v1, v2));
+
+        assertTrue(graph.containsEdge(v1, v2));
+        assertTrue(graph.containsEdge(v2, v1));
+        assertTrue(graph.containsVertex(v1));
+        assertTrue(graph.containsVertex(v2));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddDuplicateEdge() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+
+        assertFalse(graph.addEdge(v1, v2));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveVertex() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+
+        assertTrue(graph.removeVertex(v1));
+        assertFalse(graph.containsVertex(v1));
+        assertFalse(graph.containsEdge(v1, v2));
+        assertFalse(graph.containsEdge(v3, v1));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveNonExistingVertex() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+
+        assertFalse(graph.removeVertex(v8));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveEdge() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+
+        assertTrue(graph.removeEdge(v3, v4));
+        assertFalse(graph.containsEdge(v3, v4));
+        assertFalse(graph.containsEdge(v4, v3));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testNonExistingEdge() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+
+        assertFalse(graph.removeEdge(v1, v6));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetVertices() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+        graph.addVertex(v7);
+
+        assertEquals(Set.of(v1, v2, v3, v4, v7), graph.getVertices());
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetEdges() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+        graph.addVertex(v7);
+
+        assertEquals(Set.of(new Edge<>(v1, v2), new Edge<>(v2, v1), new Edge<>(v1, v3), new Edge<>(v3, v1), new Edge<>(v3, v4), new Edge<>(v4, v3)), graph.getEdges());
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetNeighbors() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+        graph.addVertex(v7);
+
+        assertEquals(Set.of(v2, v3), graph.getNeighborhood(v1));
+        assertEquals(Set.of(), graph.getNeighborhood(v7));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetNonExistingVertexNeighbors() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v1, v3);
+        graph.addEdge(v4, v3);
+        graph.addVertex(v7);
+
+        assertEquals(Set.of(), graph.getNeighborhood(v8));
+
+        assertTrue(checkInv());
+    }
 }
--- homework2/hw2-problem2/src/test/java/edu/postech/csed332/homework2/AbstractMutableTreeTest.java
+++ homework2/hw2-problem2/src/test/java/edu/postech/csed332/homework2/AbstractMutableTreeTest.java
@@ -3,6 +3,10 @@ package edu.postech.csed332.homework2;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -37,4 +41,240 @@ public abstract class AbstractMutableTreeTest<V extends Comparable<V>, T extends
 
     // TODO: write black-box test cases for each method of MutableTree with respect to
     //  the specification, including the methods of Tree that MutableTree extends.
+    @Test
+    void testGetRoot() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+
+        assertFalse(List.of(v1, v2, v3, v4, v5, v6, v7, v8).contains(tree.getRoot()));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetHeight() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+
+        assertEquals(tree.getHeight(), 2);
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetChildren() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertEquals(Set.of(v2, v4), tree.getChildren(v1));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetNonExistingVertexChildren() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertEquals(Set.of(), tree.getChildren(v6));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetParent() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertEquals(tree.getParent(v2), Optional.of(v2));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetParentRoot() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertEquals(tree.getParent(tree.getRoot()), Optional.empty());
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetParentNonExistingVertex() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertEquals(tree.getParent(v6), Optional.empty());
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddVertex() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertTrue(tree.addVertex(v5));
+
+        assertFalse(checkInv());
+    }
+
+    @Test
+    void testAddDuplicateVertex() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertFalse(tree.addVertex(v1));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveVertexIllegalArgumentException() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertThrows(IllegalArgumentException.class, () -> tree.removeVertex(tree.getRoot()));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveVertex() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertTrue(tree.removeVertex(v1));
+        assertFalse(tree.containsVertex(v2));
+        assertFalse(tree.containsVertex(v4));
+        assertFalse(tree.containsEdge(v1, v2));
+        assertFalse(tree.containsEdge(v4, v1));
+        assertFalse(tree.containsEdge(tree.getRoot(), v1));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveNonExistingVertex() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertFalse(tree.removeVertex(v6));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceInVTargetInV() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertFalse(tree.addEdge(v1,v4));
+        assertFalse(tree.containsEdge(v4, v1));
+        assertFalse(tree.containsEdge(v1, v4));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceInVTargetNotInV() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertTrue(tree.addEdge(v4, v6));
+        assertTrue(tree.containsVertex(v6));
+        assertTrue(tree.containsEdge(v4, v6));
+        assertTrue(tree.containsEdge(v6, v4));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceNotInVTargetInV() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertFalse(tree.addEdge(v6,v4));
+        assertFalse(tree.containsVertex(v6));
+        assertFalse(tree.containsEdge(v4, v6));
+        assertFalse(tree.containsEdge(v6, v4));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdgeSourceNotInVTargetNotInV() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertFalse(tree.addEdge(v6,v5));
+        assertFalse(tree.containsVertex(v6));
+        assertFalse(tree.containsVertex(v5));
+        assertFalse(tree.containsEdge(v5, v6));
+        assertFalse(tree.containsEdge(v6, v5));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveEdge() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertTrue(tree.removeEdge(tree.getRoot(), v1));
+        assertFalse(tree.containsVertex(v1));
+        assertFalse(tree.containsVertex(v2));
+        assertFalse(tree.containsVertex(v4));
+        assertFalse(tree.containsEdge(tree.getRoot(), v1));
+        assertFalse(tree.containsEdge(v1, v2));
+        assertFalse(tree.containsEdge(v1, v4));
+
+        assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveNonExistingEdge() {
+        tree.addEdge(tree.getRoot(), v1);
+        tree.addEdge(v1, v2);
+        tree.addEdge(tree.getRoot(), v3);
+        tree.addEdge(v1, v4);
+
+        assertFalse(tree.removeEdge(v6, v8));
+
+        assertTrue(checkInv());
+    }
 }
